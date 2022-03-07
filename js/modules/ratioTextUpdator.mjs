ad64eb14379c495b7589e522f5372eb8d6f7ec41
toshimaru <me@toshimaru.net>
2022-03-08 01:25:14 +0900

    Add flashBackground JS code

--- js/modules/ratioTextUpdator.mjs
+++ js/modules/ratioTextUpdator.mjs
@@ -7,11 +7,25 @@ export default class RatioTextUpdator {
 			this.constructor.#boxes.forEach((box) => {
 				const ratioBaseText = document.getElementById(`${box}_${mode}`);
 				ratioBaseText.innerText = width_or_height;
+				this.flashBackground(ratioBaseText);
+
 				const ratioText = document.getElementById(`${box}_${antiMode}`);
 				ratioText.innerText = ratio[box]();
+				this.flashBackground(ratioText);
 			});
 		})
 	}
 
+	flashBackground(el) {
+		if (el.classList.contains(this.constructor.#className)) {
+			el.style.animation = 'none';
+			el.offsetWidth;
+			el.style.animation = null;
+		} else {
+			el.classList.add(this.constructor.#className);
+		}
+	}
+
 	static #boxes = ['sixteenToNine', 'goldenRatio', 'threeToTwo', 'fourToThree'];
+	static #className = 'flash-bg'
 }
